9852510ad9bacd0973adb7eb7758faa1a8d79f58
Matthew Smith <matt@nearapogee.com>
2012-10-13 13:47:49 -0700

    initial test sinatra app.

--- Rakefile
+++ Rakefile
@@ -4,11 +4,16 @@ require 'rubygems'
 require 'hoe'
 
 Hoe.plugin :git
+Hoe.plugin :isolate
 
 Hoe.spec 'alke' do
   developer("Matt Smith", "matt@nearapogee.com")
 
-  dependency('faraday', '> 0.8.0')
+  dependency('faraday', '~> 0.8.0')
+  dependency('json', '~> 1.7.5')
+  dependency('sinatra', '~> 1.3.3', :dev)
+  dependency('sinatra-contrib', '~> 1.3.1', :dev)
+  dependency('sequel', '~> 3.4.0', :dev)
 end
 
 # vim: syntax=ruby
---
+++ test/sinatra_app.rb
@@ -0,0 +1,41 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'sinatra/json'
+require 'sequel'
+
+DB ||= Sequel.sqlite
+DB.create_table? :widgets do
+  primary_key :id
+  String :name
+  Integer :price
+  Boolean :stock
+  DateTime :updated_at
+end
+class Widget < Sequel::Model; plugin :json_serializer, naked: true; end
+Widget.find_or_create(name: 'WonderKnife', price: 1000, stock: true, updated_at: Time.utc(2012, 1, 15, 13, 30))
+
+before '/widget*' do
+  content_type :json
+end
+
+get '/', provides: :html do
+  'Alke Sinatra Testing App - There is a widgets resource here at /widgets!'
+end
+
+get '/widgets/?', provides: [:json, :html] do
+  Widget.all.to_json
+end
+
+get '/widgets/:id', provides: [:json, :html] do |id|
+  Widget[id].to_json
+end
+
+post '/widgets/?', provides: :json do
+  data = JSON.parse(request.body.read)
+  Widget.create(data['widget'].merge(updated_at: Time.now)).to_json
+end
+
+put '/widgets/:id', provides: :json do |id|
+  data = JSON.parse(request.body.read)
+  Widget[id].update(data['widget'].merge(updated_at: Time.now)).to_json
+end
